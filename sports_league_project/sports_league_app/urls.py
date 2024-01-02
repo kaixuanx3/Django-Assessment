e8d4c797bebad6dad96b521331179ca9f568ff6e
kaixuanx3 <64111495+kaixuanx3@users.noreply.github.com>
2024-01-02 20:24:17 +0800

    Update urls.py

--- sports_league_project/sports_league_app/urls.py
+++ sports_league_project/sports_league_app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', upload_csv, name='upload_csv'),
     path('games/', game_list, name='game_list'),
     path('games/add/', add_game, name='add_game'),
-    path('games/edit/', edit_game, name='edit_game'),
-    path('games/delete/', delete_game, name='delete_game'),
+    path('games/edit/<str:team_name>/', edit_game, name='edit_game'),
+    path('games/delete/<str:team_name>/', delete_game, name='delete_game'),
 ]
